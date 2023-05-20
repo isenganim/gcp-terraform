8c9c611802673fb32e6bea92451a05aa8f14f50a
isenganim <042158404@ecampus.ut.ac.id>
2023-05-20 19:13:41 +0700

    init code

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket      = "djelajah-tf"
     prefix      = "terraform/state/instances"
-    credentials = "./djelajah-68250f2c6e00.json"
+    credentials = "./file.json"
   }
 }
